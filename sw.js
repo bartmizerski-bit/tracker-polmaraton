60c33bf2a0f1fd84ff938a8003de959a1e17ac7d
bartmizerski-bit <bart.mizerski@gmail.com>
2026-08-07 06:16:27 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Service worker
+// Service worker. 
 // Wersja cache'a: podbij ten numer przy każdej większej zmianie plików,
 // żeby przeglądarka pobrała świeżą wersję zamiast starej z pamięci.
 const CACHE_NAME = "do-startu-v17";
